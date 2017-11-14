0756ac75325354de61ac0c0b87e826aca924d2bd
Lego Chiang <qas612820704@gmail.com>
2017-11-14 14:03:47 +0800

    Add WateringCan control

---
+++ Assets/Scripts/WateringCanControl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WateringCanControl : MonoBehaviour {
+
+	public float wateringAngle;
+
+	public enum State { Idle, Watering };
+	public State state;
+
+	private Vector3 vecOrigUp;
+	private Vector3 vecOrigRight;
+	private ParticleSystem particleSystem;
+
+	// Use this for initialization
+	void Start () {
+		state = State.Idle;
+
+		vecOrigUp = transform.up;
+		vecOrigRight = transform.right;
+
+		particleSystem = GetComponentInChildren<ParticleSystem>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float angleY = Vector3.Angle(vecOrigUp, transform.up);
+		float angleX = Vector3.Angle(vecOrigRight, transform.right);
+
+		if (angleX > wateringAngle && angleY > wateringAngle) {
+			Watering();
+		} else {
+			Idle();
+		}
+
+	}
+
+	void Watering() {
+		if (state == State.Watering) return;
+		
+		state = State.Watering;
+		particleSystem.Play();
+	}
+
+	void Idle() {
+		if (state == State.Idle) return;
+
+		state = State.Idle;
+		particleSystem.Stop();
+	}
+
+}
