52920b1320b0fa10ef4d5d1fa727bdb75a22df45
DESKTOP-COIOO24\user <rsp93050420@gmail.com>
2017-11-20 13:54:42 +0800

    All Stash

--- Assets/Packages/Wood texture floor_01/readme.txt
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1a8d050ed44da892c3031e2f874eb288077237171617486a0af278f5b7aa098d
-size 411
--- Assets/Scripts/PillowControl.cs
+++ Assets/Scripts/PillowControl.cs
@@ -4,9 +4,35 @@ using UnityEngine;
 
 public class PillowControl : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+    public GameObject Seed;
+    public GameObject BabyForm;
+
+    public Material StrongerIdle;
+    public Material StrongerSmile;
+    public Material StrongerHappy;
+    public Material StrongerAngry;
+    public Material StrongerSad;
+
+    public Material ChampionIdle;
+    public Material ChampionSmile;
+    public Material ChampionHappy;
+    public Material ChampionAngry;
+    public Material ChampionSad;
+
+    public Material Teacher;
+    public Material Normal;
+    public Material Fail;
+
+    private Renderer rend;
+
+    // Use this for initialization
+    void Start () {
+        rend = gameObject.GetComponent<Renderer>();
+        rend.material = StrongerIdle;
+        rend.enabled = false;
+        Seed.SetActive(true);
+        Seed.transform.position = gameObject.transform.position;
+        BabyForm.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -14,8 +40,82 @@ public class PillowControl : MonoBehaviour {
 		
 	}
 
-	void OnParticleCollision(GameObject other) {
-		Debug.Log("Col Hit!");
-	}
+    public void BecomeBaby()
+    {
+        Seed.GetComponent<Renderer>().enabled = false;
+        BabyForm.SetActive(true);
+        BabyForm.transform.position = Seed.transform.position;
+    }
 
+    public void BecomeStronger()
+    {
+        BabyForm.SetActive(false);
+        gameObject.GetComponent<Renderer>().enabled = true;
+        gameObject.transform.position = BabyForm.transform.position;
+        ChangeMaterial(0, 0);
+    }
+
+    public void ChangeMaterial(int form, int type)
+    {
+        if (form == 0)
+        {
+            switch (type)
+            {
+                case 0:
+                    rend.material = StrongerIdle;
+                    break;
+                case 1:
+                    rend.material = StrongerSmile;
+                    break;
+                case 2:
+                    rend.material = StrongerHappy;
+                    break;
+                case 3:
+                    rend.material = StrongerAngry;
+                    break;
+                case 4:
+                    rend.material = StrongerSad;
+                    break;
+            }
+        }
+        else if (form == 1)
+        {
+            switch (type)
+            {
+                case 0:
+                    rend.material = ChampionIdle;
+                    break;
+                case 1:
+                    rend.material = ChampionSmile;
+                    break;
+                case 2:
+                    rend.material = ChampionHappy;
+                    break;
+                case 3:
+                    rend.material = ChampionAngry;
+                    break;
+                case 4:
+                    rend.material = ChampionSad;
+                    break;
+            }
+        }
+        else
+        {
+            switch (type)
+            {
+                case 0:
+                    rend.material = Fail;
+                    break;
+                case 1:
+                    rend.material = Normal;
+                    break;
+                case 2:
+                    rend.material = Teacher;
+                    break;
+                default:
+                    rend.material = Fail;
+                    break;
+            }
+        }
+    }
 }
